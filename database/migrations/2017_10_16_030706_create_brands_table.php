07a0bb90b97edf55c4efc065b89b70c2dc9b2cf9
dev_tuan_nm <manhtuanzai@gmail.com>
2017-10-17 08:29:44 +0700

    update migration 01

---
+++ database/migrations/2017_10_16_030706_create_brands_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBrandsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brands', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable()->comment('Tên Hãng');
+            $table->string('description')->nullable()->comment('Mô tả');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('brands');
+    }
+}
---
+++ database/migrations/2017_10_16_073536_create_notifications_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->nullable()->comment('Tiêu đề');
+            $table->text('comment')->nullable()->comment('Nội dung');
+            $table->integer('user_id')->unsigned()->nullable()->comment('User id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notification');
+    }
+}
