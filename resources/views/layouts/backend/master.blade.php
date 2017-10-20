1c49185db0517269660ff8f32f6165b5ca91b704
dev_tuan_nm <manhtuanzai@gmail.com>
2017-10-20 13:17:37 +0700

    init layout backend

---
+++ resources/views/layouts/backend/master.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+    <head>
+        <title>Admin</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+        <link rel="stylesheet" type='text/css' href="{{ Url('bower_components/bootstrap/dist/css/bootstrap.min.css') }}" >
+        <link rel='stylesheet' type='text/css' href="{{ Url('css/backend/style.css') }}" />
+        <link rel='stylesheet' type='text/css' href="{{ Url('css/backend/style-responsive.css') }}" />
+        <link rel='stylesheet' type='text/css' href="{{ Url('css/backend/font-custom.css') }}" />
+        <link rel="stylesheet" type="text/css" href="{{ Url('css/backend/font.css') }}"/>
+        <link rel="stylesheet" type="text/css" href="{{ Url('bower_components/font-awesome/css/font-awesome.css') }}" rel="stylesheet">
+        <link rel='stylesheet' type='text/css' href="{{ Url('css/backend/morris.css') }}" />
+        <link rel='stylesheet' type='text/css' href="{{ Url('css/backend/monthly.css') }}" />
+    </head>
+    <body>
+        <section id="container">
+            @include('layouts.backend.partial.header')
+            @include('layouts.backend.partial.sidebar')
+            @yield('content')
+        </section>
+        <script src="{{ Url('bower_components/my-jquery/jquery.js') }}"></script>
+        <script src="{{ Url('js/backend/raphael-min.js') }}"></script>
+        <script src="{{ Url('js/backend/morris.js') }}"></script>
+        <script src="{{ Url('bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+        <script src="{{ Url('bower_components/dcjqaccordion/js/jquery.dcjqaccordion.2.7.js') }}"></script>
+        <script src="{{ Url('js/backend/scripts.js') }}"></script>
+        <script src="{{ Url('bower_components/jquery-slimscroll/jquery.slimscroll.js') }}"></script>
+        <script src="{{ Url('bower_components/jquery.nicescroll/dist/jquery.nicescroll.js') }}"></script>
+        <script src="{{ Url('js/backend/jquery.scrollTo.js') }}"></script>
+    </body>
+</html>
